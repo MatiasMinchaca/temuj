11564eef5a3e9305cf95c67a2411ec683f724bb6
MatiasMinchaca <matiasminchaca1@gmail.com>
2023-09-14 05:31:03 -0300

    varius changes

---
+++ components/WhatsAppButton/WhatsApp Elements.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    position: fixed;
+    width: 50px;
+    height: 50px;
+    bottom: 5px;
+    left: 5px;
+    img{
+        filter: drop-shadow(0 2px 6px #000000a6);
+    }
+`;
